30a071248b0e8e40c063bf1251a9fb45ab05e6ae
Summer Brochtrup & Momo Ozawa <student@epicodus.com>
2015-08-26 16:29:17 -0700

    Add new restaurant output route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -37,13 +37,10 @@ public class App {
 
       String name = request.queryParams("name");
       Integer price_id = Integer.parseInt(request.queryParams("price_id"));
-      //Integer cuisine_id = Integer.parseInt(request.queryParams("cuisine"));
-      Restaurant newRestaurant = new Restaurant(name, 1, price_id);
+      Integer cuisine_id = Integer.parseInt(request.queryParams("cuisine_id"));
+      Restaurant newRestaurant = new Restaurant(name, cuisine_id, price_id);
       newRestaurant.save();
 
-
-      //Integer price_range = Integer.parseInt(request.queryParams("price_range"));
-      //Integer cuisine_type = Integer.parseInt(request.queryParams(""));
       model.put("restaurants", Restaurant.all());
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
